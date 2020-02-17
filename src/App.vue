4f53017aa7c1c7127f61eeb61be53445ff9258b7
NmVAson <giulswatson@gmail.com>
2020-02-17 13:32:22 -0500

    Create ExperienceSamplingMethodForm

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Flow logo" src="./assets/logo.png">
+    <ExperienceSamplingMethodForm msg="FLOW"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ExperienceSamplingMethodForm from './components/ExperienceSamplingMethodForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    ExperienceSamplingMethodForm,
   },
 };
 </script>
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  width: 200px;
 }
 </style>
--- tests/unit/ExperienceSamplingMethodForm.spec.js
+++ tests/unit/ExperienceSamplingMethodForm.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import ExperienceSamplingMethodForm from '@/components/ExperienceSamplingMethodForm.vue';
 
-describe('HelloWorld.vue', () => {
+describe('ExperienceSamplingMethodForm.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(ExperienceSamplingMethodForm, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
