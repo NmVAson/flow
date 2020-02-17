324e8532428b54d8858a87047f1f59476601f2ec
Giuliana Taylor <giulswatson@gmail.com>
2020-02-17 18:03:15 -0500

    Extract form inputs to its own component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-form-group
+    :label-for=id
+    :label=label
+    label-cols-sm="5"
+    label-align-sm="right">
+    <b-form-input :id=id :type=type :v-model=model />
+    </b-form-group>
+</template>
+
+<script>
+export default {
+  name: 'FormInput',
+  props: {
+    id: String,
+    type: String,
+    label: String,
+    model: String,
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/IntroSection.vue
+++ src/components/IntroSection.vue
@@ -1,74 +1,37 @@
 <template>
   <FormCard title="Contextualize the Activity">
-      <b-form-group
-        label-for="datetime"
-        label="Date"
-        label-cols-sm="5"
-        label-align-sm="right">
-        <b-form-input id="datetime" type="datetime-local" v-model="datetime" disabled/>
-      </b-form-group>
-
-      <b-form-group
-        label-for="thought"
-        label="What were you thinking about?"
-        label-cols-sm="5"
-        label-align-sm="right">
-        <b-form-input id="thought" type="text" v-model="thought"/>
-      </b-form-group>
-
-      <b-form-group
-        label-for="place"
-        label="Where were you?"
-        label-cols-sm="5"
-        label-align-sm="right">
-        <b-form-input id="place" type="text" v-model="place"/>
-      </b-form-group>
-
-      <b-form-group
-        label-for="action"
-        label="What was the main thing you were doing?"
-        label-cols-sm="5"
-        label-align-sm="right">
-        <b-form-input id="action" type="text" v-model="action"/>
-      </b-form-group>
-
-      <b-form-group
-        label-for="why"
-        label="Why were you doing this particular activity?"
-        label-cols-sm="5"
-        label-align-sm="right">
-        <b-form-radio-group
-          id="why"
-          v-model="why"
-          :options="whyOptions"
-        ></b-form-radio-group>
-      </b-form-group>
-    </FormCard>
+    <FormInput id="datetime" label="Date" type="datetime-local"/>
+    <FormInput id="thought" label="What were you thinking about?" type="text"/>
+    <FormInput id="place" label="Where were you?" type="text"/>
+    <FormInput id="action" label="What was the main thing you were doing?" type="text"/>
+    <b-form-group
+      label-for="why"
+      label="Why were you doing this particular activity?"
+      label-cols-sm="5"
+      label-align-sm="right">
+      <b-form-radio-group
+        id="why"
+        :options="whyOptions"
+      ></b-form-radio-group>
+    </b-form-group>
+  </FormCard>
 </template>
 
 <script>
-import moment from 'moment';
 import FormCard from './FormCard.vue';
+import FormInput from './FormInput.vue';
 
 export default {
   name: 'IntroSection',
   props: {
     title: String,
-    datetime: {
-      type: Date,
-      default: () => moment().format('YYYY-MM-DDTHH:mm:ss'),
-    },
-    thought: String,
-    place: String,
-    action: String,
-    why: String,
-    whyOptions: {
-      type: Array,
-      default: () => ['I had to', 'I wanted to', 'I had nothing else to do'],
-    },
   },
+  data: () => ({
+    whyOptions: ['I had to', 'I wanted to', 'I had nothing else to do'],
+  }),
   components: {
     FormCard,
+    FormInput,
   },
 };
 </script>
